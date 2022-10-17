0c0bc3f45550fa753ded1a70ebb26001b900c7ce
Kai Shing <kshing21@students.psc.ac.uk>
2022-10-17 11:30:33 +0100

    Settings Form

--- PRJ_MazeWinForms/MenuForm.Designer.cs
+++ PRJ_MazeWinForms/MenuForm.Designer.cs
@@ -28,30 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuLayout = new System.Windows.Forms.TableLayoutPanel();
             this.btn_start = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuLayout.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // menuLayout
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
-            this.tableLayoutPanel1.Controls.Add(this.btn_start, 1, 1);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.menuLayout.ColumnCount = 3;
+            this.menuLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.menuLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.menuLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.menuLayout.Controls.Add(this.btn_start, 1, 1);
+            this.menuLayout.Controls.Add(this.label1, 1, 0);
+            this.menuLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.menuLayout.Location = new System.Drawing.Point(0, 0);
+            this.menuLayout.Name = "menuLayout";
+            this.menuLayout.RowCount = 3;
+            this.menuLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.menuLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.menuLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.menuLayout.Size = new System.Drawing.Size(800, 450);
+            this.menuLayout.TabIndex = 0;
             // 
             // btn_start
             // 
+            this.btn_start.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_start.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.btn_start.Location = new System.Drawing.Point(286, 170);
@@ -62,23 +65,37 @@
             this.btn_start.Text = "Start";
             this.btn_start.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.label1.Location = new System.Drawing.Point(269, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 150);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Maze";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
             // MenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuLayout);
             this.Name = "MenuForm";
             this.Text = "Menu";
-            this.tableLayoutPanel1.ResumeLayout(false);
+            this.menuLayout.ResumeLayout(false);
+            this.menuLayout.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel menuLayout;
         private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- PRJ_MazeWinForms/SettingsForm.Designer.cs
+++ PRJ_MazeWinForms/SettingsForm.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.settingsLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_generate = new System.Windows.Forms.Button();
+            this.lbl_settings = new System.Windows.Forms.Label();
+            this.settingsLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // settingsLayout
+            // 
+            this.settingsLayout.ColumnCount = 5;
+            this.settingsLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.settingsLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.settingsLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.settingsLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.settingsLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.settingsLayout.Controls.Add(this.btn_generate, 2, 4);
+            this.settingsLayout.Controls.Add(this.lbl_settings, 2, 0);
+            this.settingsLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.settingsLayout.Location = new System.Drawing.Point(0, 0);
+            this.settingsLayout.Name = "settingsLayout";
+            this.settingsLayout.RowCount = 5;
+            this.settingsLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.settingsLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.settingsLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.settingsLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.settingsLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.settingsLayout.Size = new System.Drawing.Size(800, 450);
+            this.settingsLayout.TabIndex = 0;
+            // 
+            // btn_generate
+            // 
+            this.btn_generate.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_generate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btn_generate.Location = new System.Drawing.Point(330, 370);
+            this.btn_generate.Margin = new System.Windows.Forms.Padding(10);
+            this.btn_generate.Name = "btn_generate";
+            this.btn_generate.Size = new System.Drawing.Size(140, 70);
+            this.btn_generate.TabIndex = 0;
+            this.btn_generate.Text = "Generate";
+            this.btn_generate.UseVisualStyleBackColor = true;
+            // 
+            // lbl_settings
+            // 
+            this.lbl_settings.AutoSize = true;
+            this.lbl_settings.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl_settings.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lbl_settings.Location = new System.Drawing.Point(323, 0);
+            this.lbl_settings.Name = "lbl_settings";
+            this.lbl_settings.Size = new System.Drawing.Size(154, 90);
+            this.lbl_settings.TabIndex = 1;
+            this.lbl_settings.Text = "Settings";
+            this.lbl_settings.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.settingsLayout);
+            this.Name = "SettingsForm";
             this.Text = "SettingsForm";
+            this.settingsLayout.ResumeLayout(false);
+            this.settingsLayout.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel settingsLayout;
+        private System.Windows.Forms.Button btn_generate;
+        private System.Windows.Forms.Label lbl_settings;
     }
 }
